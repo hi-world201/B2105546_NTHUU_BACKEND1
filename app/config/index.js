381f638138017593b606124803896d979c2d5de5
hi-world201 <90491406+hi-world201@users.noreply.github.com>
2023-09-10 18:36:17 +0700

    Cai dat xu ly loi

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,7 @@
 const config = {
-    app: {
-        port: process.env.PORT || 3000,
-    }
+  app: {
+    port: process.env.PORT || 3000,
+  },
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 const config = require('./app/config');
 
-// Start server 
+// Start server
 const PORT = config.app.port;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
