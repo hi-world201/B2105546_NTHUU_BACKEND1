22268ca4b7af90e37cfb9f709a336141b7c2e600
hi-world201 <90491406+hi-world201@users.noreply.github.com>
2023-09-10 18:23:24 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contack book application.' });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to contack book application.",
+  });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
